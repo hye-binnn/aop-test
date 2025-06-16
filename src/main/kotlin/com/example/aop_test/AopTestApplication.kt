34cd9846adf08a082bd51482bddd84915acbe2a0
hye-binnn <hyebin.kim@pfct.co.kr>
2025-06-16 11:32:06 +0900

    init

---
+++ src/main/kotlin/com/example/aop_test/AopTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.aop_test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AopTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<AopTestApplication>(*args)
+}
